47893dcf9109e055fe72de09d31a34df82bd22b9
dannydebug <danica.v90@gmail.com>
2020-06-27 19:29:45 +0200

    Fix lint issues

--- packages/frontend/src/components/NavBar.vue
+++ packages/frontend/src/components/NavBar.vue
@@ -7,11 +7,19 @@
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
-            <b-form-input size="md" class="mr-sm-2" placeholder="Search by account / token"></b-form-input>
-            <b-button size="md" class="my-2 my-sm-0" type="submit">Search</b-button>
+            <b-form-input
+              size="md"
+              class="mr-sm-2"
+              placeholder="Search by account / token"
+            ></b-form-input>
+            <b-button size="md" class="my-2 my-sm-0" type="submit">
+              Search
+            </b-button>
           </b-nav-form>
           <b-nav-item>Log In</b-nav-item>
-          <b-nav-item href="/account/:id" active-class="active">User</b-nav-item>
+          <b-nav-item href="/account/:id" active-class="active">
+            User
+          </b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
