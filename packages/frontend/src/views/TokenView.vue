daaf3f3c997c5927d6806716b3789ba61e6adaf4
dannydebug <danica.v90@gmail.com>
2020-07-04 21:17:36 +0200

    Add token view

---
+++ packages/frontend/src/views/TokenView.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-container class="py-5">
+    <div class="d-flex align-items-center">
+      <Avatar size="4em" :value="token" type="token" />
+      <h2 class="m-0 ml-3">{{ token }}</h2>
+    </div>
+    <hr />
+    <TokenInfo :token="token" />
+  </b-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import TokenInfo from "../components/TokenInfo.vue";
+import Avatar from "../components/Avatar.vue";
+
+@Component({
+  components: { TokenInfo, Avatar }
+})
+export default class TokenView extends Vue {
+  @Prop({ required: true })
+  token!: string;
+}
+</script>
+
+<style></style>
