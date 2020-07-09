386b0958c5a5ff3ee8ad06dcf158679990ed979d
dannydebug <danica.v90@gmail.com>
2020-07-09 23:36:16 +0200

    Write v-if and v-else in a cleaner way inside of the components

--- packages/frontend/src/components/TokenHolders.vue
+++ packages/frontend/src/components/TokenHolders.vue
@@ -4,29 +4,27 @@
       <b-spinner label="Spinning"></b-spinner>
       Loading...
     </div>
-    <div v-else>
-      <div v-if="users && users.length === 0">
-        <p>There are no token holders of this token.</p>
-      </div>
-      <b-list-group v-else>
-        <b-list-group-item v-for="user in users" :key="user.account">
-          <router-link
-            :to="{ name: 'account', params: { account: user.account } }"
-          >
-            <Avatar size="2em" :value="user.account" type="account" />
-            {{ user.account }}
-          </router-link>
-          <b-icon icon="arrow-right"></b-icon>
-          <router-link
-            :to="{
-              name: 'token',
-              params: { token: user.balance.split(' ')[1] }
-            }"
-            ><span class="mx-2">{{ user.balance }}</span>
-          </router-link>
-        </b-list-group-item>
-      </b-list-group>
+    <div v-else-if="users && users.length === 0">
+      <p>There are no token holders of this token.</p>
     </div>
+    <b-list-group v-else>
+      <b-list-group-item v-for="user in users" :key="user.account">
+        <router-link
+          :to="{ name: 'account', params: { account: user.account } }"
+        >
+          <Avatar size="2em" :value="user.account" type="account" />
+          {{ user.account }}
+        </router-link>
+        <b-icon icon="arrow-right"></b-icon>
+        <router-link
+          :to="{
+            name: 'token',
+            params: { token: user.balance.split(' ')[1] }
+          }"
+          ><span class="mx-2">{{ user.balance }}</span>
+        </router-link>
+      </b-list-group-item>
+    </b-list-group>
   </div>
 </template>
 
