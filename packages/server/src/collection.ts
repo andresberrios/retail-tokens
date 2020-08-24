783d989825e2a95be119d93546e35524cf4b73d0
andresberrios <andres.berrios.j@gmail.com>
2020-08-24 12:25:55 +0200

    Configure DB connection for Atlas cluster

--- packages/server/src/collection.ts
+++ packages/server/src/collection.ts
@@ -4,7 +4,10 @@ const url = process.env.MONGO_URL || "";
 const collectionName = "registrations";
 
 export async function getCollection() {
-  const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  const client = await MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
   const collection = client.db().collection(collectionName);
   collection.createIndex(
     { token: 1, account: 1 },
